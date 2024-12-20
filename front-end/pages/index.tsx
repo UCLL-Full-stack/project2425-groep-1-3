c4850a23aaffdb6a8cd34b93b6f88c965d450915
Thomas Covens <thomas.covens@student.ucll.be>
2024-12-20 22:21:18 +0100

    home page

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -13,15 +13,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header></Header>
-      <main>
-        <h1>welcome to the home page</h1>
+      <Header />
+      <main className="flex items-center justify-center h-screen">
+        <h1 className="text-4xl bg-[#31b8f7] p-6 rounded-lg shadow-lg">
+          Welcome to the home page
+        </h1>
       </main>
     </>
   );
 }
 
-export const getServerSideProps = async (context: { locale: any; }) => {
+export const getServerSideProps = async (context: { locale: any }) => {
   const { locale } = context;
   return {
     props: {
